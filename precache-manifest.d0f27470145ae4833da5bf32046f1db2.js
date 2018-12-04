2ab31fba21489f49fc4163393bb708ed9baab781
MalgoskaG <ghouga.m@gmail.com>
2018-12-04 20:31:07 +0100

    Updates

---
+++ precache-manifest.d0f27470145ae4833da5bf32046f1db2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe26dc10219f9e3ecc30",
+    "url": "/trackingTest/static/js/runtime~main.fe26dc10.js"
+  },
+  {
+    "revision": "acc9112512c8f74080f9",
+    "url": "/trackingTest/static/js/main.acc91125.chunk.js"
+  },
+  {
+    "revision": "e93fa1e395f9999c35e3",
+    "url": "/trackingTest/static/js/1.e93fa1e3.chunk.js"
+  },
+  {
+    "revision": "acc9112512c8f74080f9",
+    "url": "/trackingTest/static/css/main.d1f0a61f.chunk.css"
+  },
+  {
+    "revision": "9572132d2f8d53bbfdcccb5c3db51102",
+    "url": "/trackingTest/index.html"
+  }
+];
